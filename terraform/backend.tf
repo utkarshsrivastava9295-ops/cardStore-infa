e10dee064dc9d51dd0dbe9b725852883a75a3bee
Utkarsh Srivastava <utkarsh.srivastava9295@gmail.com>
2026-06-28 00:37:22 +0530

    updated pipeline

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name = "terraform-rg"
     storage_account_name = "utkarshterraformms"
     container_name = "terraformstate"
-    key = "${var.environment}/terraform.tfstate"
+    # key = "${var.environment}/terraform.tfstate"
   }
 }
